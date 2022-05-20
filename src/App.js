179fc3735b64c1f1f282c55d222ab5698008c9df
Bruno Perkovic <brunko10@gmail.com>
2022-05-20 13:13:43 +0200

    dodane karte kako triba i dodana neka stanja

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
 import "./App.scss";
 import { useState } from "react";
-import PokerCard from "./Components/pokerCard/PokerCard";
+import PokerCard from "./Components/PokerCard/PokerCard";
 import club from "./Images/club.jpg";
 import diamond from "./Images/diamond.jpg";
 import heart from "./Images/heart.jpg";
 import spade from "./Images/spade.jpg";
 import pokerback from "./Images/pokerback.jpg";
+import Card from "react-playing-card";
+
+const suits = ["H", "D", "S", "C"];
+const ranks = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+
+const deck = [];
+suits.forEach((suit) => {
+  ranks.forEach((rank, i) => {
+    deck.push(<Card suit={suit} rank={rank} key={i} />);
+  });
+});
+console.log(deck);
 
 function App() {
   const [selectCard, setSelectCard] = useState();
   const [remainingCards, setRemainingCards] = useState(53);
   const [score, setScore] = useState(0);
+  const [poppedCard, setPoppedCard] = useState();
 
-  const handleSelectCard = (color) => {
-    alert("you clicked me. My suit is: " + color);
+  const showGeneratedCard = () => {
+    setPoppedCard(deck.splice(Math.floor(Math.random() * deck.length), 1));
+    console.log(poppedCard);
   };
 
-  const handleRemainingCards = () => {
+  const handleSelectCard = (color) => {
     setRemainingCards(remainingCards - 1);
+    showGeneratedCard();
   };
 
   const handleScore = () => {
     setScore(score + 1);
   };
+
   return (
     <div className="App">
-      <div className="card">
-        <div className="card__option">
-          <div className="card__option--row">
+      <div className="mojCard">
+        <div className="mojCard__option">
+          <div className="mojCard__option--row">
             <PokerCard suit={spade} action={handleSelectCard} color="spade" />
             <PokerCard suit={club} action={handleSelectCard} color="club" />
           </div>
 
-          <div className="card__option--row">
+          <div className="mojCard__option--row">
             <PokerCard suit={heart} action={handleSelectCard} color="heart" />
             <PokerCard
               suit={diamond}
@@ -42,16 +72,14 @@ function App() {
           </div>
         </div>
 
-        <div className="card__deck">
+        <div className="score">Koliko si pogodia karata: {score} </div>
+
+        <div className="mojCard__deck">
           <p>Remaining cards: {remainingCards} </p>
-          <PokerCard card={pokerback} />
-          <div className="card__deck--graveyard">
-            odigrane karte, tu ce se prikazat zadnja odigrana karta
-          </div>
+          <PokerCard suit={pokerback} />
+          <div className="mojCard__deck--graveyard">{poppedCard}</div>
         </div>
       </div>
-
-      <div className="score">Koliko si pogodia karata: {score} </div>
     </div>
   );
 }
