b2a561870ff3c70305713e0fb381cd51fb340320
Bruno Perkovic <brunko10@gmail.com>
2022-05-21 18:17:16 +0200

    izmjene classname

--- src/App.js
+++ src/App.js
@@ -11,92 +11,84 @@ import { act } from "react-dom/test-utils";
 
 const suits = ["H", "D", "S", "C"];
 const ranks = [
-	"A",
-	"2",
-	"3",
-	"4",
-	"5",
-	"6",
-	"7",
-	"8",
-	"9",
-	"10",
-	"J",
-	"Q",
-	"K",
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
 ];
 
 const deck = [];
 suits.forEach((suit) => {
-	ranks.forEach((rank, i) => {
-		deck.push(<Card suit={suit} rank={rank} key={i} />);
-	});
+  ranks.forEach((rank, i) => {
+    deck.push(<Card suit={suit} rank={rank} key={i} />);
+  });
 });
 console.log(deck);
 
 function App() {
-	const [selectCard, setSelectCard] = useState();
-	const [remainingCards, setRemainingCards] = useState(deck.length);
-	const [score, setScore] = useState(0);
-	const [poppedCard, setPoppedCard] = useState(<PokerCard suit={pokerback} />);
+  const [selectCard, setSelectCard] = useState();
+  const [remainingCards, setRemainingCards] = useState(deck.length);
+  const [score, setScore] = useState(0);
+  const [poppedCard, setPoppedCard] = useState(<PokerCard suit={pokerback} />);
 
-	const resetStates = () => {
-		setRemainingCards(deck.length);
-		setScore(0);
-		setPoppedCard(<PokerCard suit={pokerback} />);
-	};
+  const resetStates = () => {
+    setRemainingCards(deck.length);
+    setScore(0);
+    setPoppedCard(<PokerCard suit={pokerback} />);
+  };
 
-	/* 	const showGeneratedCard = () => {
-		setPoppedCard(deck.splice(Math.floor(Math.random() * deck.length), 1));
-		console.log(poppedCard);
-	}; */
+  const handleSelectCard = (color) => {
+    setPoppedCard(deck.splice(Math.floor(Math.random() * deck.length), 1));
+    setRemainingCards(remainingCards - 1);
+    if (color === "S") {
+      handleScore();
+    } else if (color === "D") {
+      handleScore();
+    } else if (color === "H") {
+      handleScore();
+    } else if (color === "C") {
+      handleScore();
+    }
+  };
 
-	const handleSelectCard = (color) => {
-		setPoppedCard(deck.splice(Math.floor(Math.random() * deck.length), 1));
-		setRemainingCards(remainingCards - 1);
-		if (color === "S") {
-			handleScore();
-		} else if (color === "D") {
-			handleScore();
-		} else if (color === "H") {
-			handleScore();
-		} else if (color === "C") {
-			handleScore();
-		}
+  const handleScore = () => {
+    setScore(score + 1);
+  };
 
-		/* 		console.log(`moja boja je ${suit}`);
-		 */
-	};
+  return (
+    <div className="App">
+      <div className="board">
+        <div className="board__option">
+          <div className="board__option--row">
+            <PokerCard suit={spade} action={handleSelectCard} color="S" />
+            <PokerCard suit={club} action={handleSelectCard} color="C" />
+          </div>
 
-	const handleScore = () => {
-		setScore(score + 1);
-	};
+          <div className="board__option--row">
+            <PokerCard suit={heart} action={handleSelectCard} color="H" />
+            <PokerCard suit={diamond} action={handleSelectCard} color="D" />
+          </div>
+        </div>
 
-	return (
-		<div className="App">
-			<div className="mojCard">
-				<div className="mojCard__option">
-					<div className="mojCard__option--row">
-						<PokerCard suit={spade} action={handleSelectCard} color="S" />
-						<PokerCard suit={club} action={handleSelectCard} color="C" />
-					</div>
+        <div className="score">Koliko si pogodia karata: {score} </div>
 
-					<div className="mojCard__option--row">
-						<PokerCard suit={heart} action={handleSelectCard} color="H" />
-						<PokerCard suit={diamond} action={handleSelectCard} color="D" />
-					</div>
-				</div>
-
-				<div className="score">Koliko si pogodia karata: {score} </div>
-
-				<div className="mojCard__deck">
-					<p>Remaining cards: {remainingCards} </p>
-					<PokerCard suit={pokerback} />
-					<div className="mojCard__deck--graveyard">{poppedCard}</div>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="board__deck">
+          <p>Remaining cards: {remainingCards} </p>
+          <PokerCard suit={pokerback} />
+          <div className="board__deck--graveyard">{poppedCard}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
