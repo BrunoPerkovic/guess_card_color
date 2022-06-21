3ad3477d653ed8dcb6d5ee8ec69cfcfc272b48be
Bruno Perkovic <brunko10@gmail.com>
2022-06-21 10:34:18 +0200

    dizajn

--- src/Components/pokerCard/PokerCard.js
+++ src/Components/pokerCard/PokerCard.js
@@ -2,14 +2,13 @@ import React from "react";
 import "./PokerCard.scss";
 const PokerCard = ({ suit, action, color }) => {
   return (
-    <a
-      href="#"
+    <button
       onClick={() => {
         action(color);
       }}
     >
       <img src={suit} alt="poker card" />
-    </a>
+    </button>
   );
 };
 
